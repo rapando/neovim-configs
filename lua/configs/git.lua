e7cefc766347be4dbb4ab674cf42a35c7538707a
Samson Rapando <sam@Samsons-MacBook-Air.local>
2024-11-11 14:08:15 +0300

    prompt lazygit to close on exit and on enter press

--- lua/configs/git.lua
+++ lua/configs/git.lua
@@ -1,1 +1,6 @@
 vim.api.nvim_set_keymap('n', '<leader>gg', ':LazyGit<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<CR>', ":q<CR>", {noremap = true, silent=true})
+
+require("lazygit").setup({
+  close_on_exit: true
+})
