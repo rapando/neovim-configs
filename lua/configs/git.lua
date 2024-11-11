c34a90934f84dfba27bc18c48eb5ba91b1f852eb
Samson Rapando <sam@Samsons-MacBook-Air.local>
2024-11-11 14:09:35 +0300

    reverted

--- lua/configs/git.lua
+++ lua/configs/git.lua
@@ -1,6 +1,1 @@
 vim.api.nvim_set_keymap('n', '<leader>gg', ':LazyGit<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<CR>', ":q<CR>", {noremap = true, silent=true})
-
-require("lazygit").setup({
-  close_on_exit: true
-})
