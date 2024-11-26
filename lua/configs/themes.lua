cd6f4a918f90714df84fca66be6a06212ef7dd49
rapando <samsonrapando@gmail.com>
2024-11-26 20:15:52 +0300

    added tablines

--- lua/configs/themes.lua
+++ lua/configs/themes.lua
@@ -12,6 +12,7 @@ vim.g['airline#extensions#tabline#left_sep'] = ' '
 vim.g['airline#extensions#tabline#left_alt_sep'] = '|'
 
 -- tablines
+--[[require("ibl").setup { indent = { highlight = highlight } }
 local highlight = {
     "RainbowRed",
     "RainbowYellow",
@@ -22,9 +23,8 @@ local highlight = {
     "RainbowCyan",
 }
 
+require("ibl").setup { indent = { highlight = highlight } }
 local hooks = require "ibl.hooks"
--- create the highlight groups in the highlight setup hook, so they are reset
--- every time the colorscheme changes
 hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
     vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
     vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
@@ -34,6 +34,7 @@ hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
     vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
     vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
 end)
-
 require("ibl").setup { indent = { highlight = highlight } }
+]]
 
+require("ibl").setup()
