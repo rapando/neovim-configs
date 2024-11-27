6190cf37d17c2a258f35ff6dd07e483f58e00be6
rapando <samsonrapando@gmail.com>
2024-11-27 12:36:28 +0300

    removed dashboards

--- lua/configs/themes.lua
+++ lua/configs/themes.lua
@@ -2,7 +2,7 @@
 vim.o.background = "dark"
 
 -- set colorscheme
-vim.cmd("colorscheme oxocarbon")
+vim.cmd("colorscheme citruszest")
 vim.opt.background = "dark"
 
 -- enable the tabline
@@ -11,30 +11,3 @@ vim.g['airline#extensions#tabline#enabled'] = 1
 vim.g['airline#extensions#tabline#left_sep'] = ' '
 vim.g['airline#extensions#tabline#left_alt_sep'] = '|'
 
--- tablines
---[[require("ibl").setup { indent = { highlight = highlight } }
-local highlight = {
-    "RainbowRed",
-    "RainbowYellow",
-    "RainbowBlue",
-    "RainbowOrange",
-    "RainbowGreen",
-    "RainbowViolet",
-    "RainbowCyan",
-}
-
-require("ibl").setup { indent = { highlight = highlight } }
-local hooks = require "ibl.hooks"
-hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-    vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-    vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-    vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-    vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-    vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-    vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-    vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
-end)
-require("ibl").setup { indent = { highlight = highlight } }
-]]
-
-require("ibl").setup()
--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,13 +1,5 @@
 return {
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		main = "ibl",
-		---@module "ibl"
-		---@type ibl.config
-		opts = {},
-	},
-    { "glepnir/dashboard-nvim" },  -- startup dashboard
-    { "vim-airline/vim-airline" }, -- status line at the bottom
+	{ "vim-airline/vim-airline" }, -- status line at the bottom
     { "vim-airline/vim-airline-themes" },
     {
         "zootedb0t/citruszest.nvim",
