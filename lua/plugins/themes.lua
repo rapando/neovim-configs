0ca19f078e989e8ced0c73511511161126d95607
rapando <samsonrapando@gmail.com>
2025-02-04 22:20:16 +0300

    changed theme

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -6,5 +6,13 @@ return {
 		"ribru17/bamboo.nvim",
 		lazy = false,
 		priority = 1000,
+	},
+	{
+		"AlexvZyl/nordic.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("nordic").load()
+		end
 	}
 }
